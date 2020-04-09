74822ba7bb3eaeeb4ba230732020ffd6b34b1a59
MonolithOfficial <MonolithOfficial@users.noreply.github.com>
2020-04-09 17:06:37 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- hello.c
+++ hello.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
+    // retrieving our dear user's input.
     string name = get_string("Bestow thy holy moniker, fair stranger\n");
+    
+    // replying to our dear holy user, so that he doesn't feel desolated.
     printf("hello, %s.\n", name);
 }
