1b9a5b45c393e8d804e4eff5bb8e0c9ddf76bc3a
MonolithOfficial <MonolithOfficial@users.noreply.github.com>
2020-04-11 14:24:00 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- cash.c
+++ cash.c
@@ -5,35 +5,27 @@
 int main (void)
 {
     // Initializing input variable
-    float input = 0;
+    float dollars = 0;
     // Making sure our dear user is forced to conform to the program.
     do
     {
-        input = get_float("Change owed: ");    
+        dollars = get_float("Change owed: ");    
     }
     // Gotta be positive to make any sort of sense, right?
-    while (input < 0);
+    while (dollars < 0);
     // A cent is one hundredth of a dollar.
-    int cents = round(input * 100);
-    // A quarter
-    float quarter = 0.25;
-    // One tenth
-    float dime = 0.1;
-    // Twice less than one tenth
-    float nickel = 0.05;
-    // Five times less than that
-    float penny = 0.01;
-    // Calculating numbers of coins in need and passing down the remainder.
-    int quartersNeeded = input / quarter;
-    float remainderChange = input - (quartersNeeded * quarter);
-    int dimesNeeded = round(remainderChange / dime);
-    remainderChange = remainderChange - (dimesNeeded * dime);
-    int nickelsNeeded = round(remainderChange / nickel);
-    remainderChange = remainderChange - (nickelsNeeded * nickel);
-    int penniesNeeded = round(remainderChange / penny);
-    remainderChange = remainderChange - (penniesNeeded * penny);
-    int coinsNeeded = round(quartersNeeded + dimesNeeded + nickelsNeeded + penniesNeeded);
-    
-    printf("%i\n", coinsNeeded);
+    int cents = round(dollars * 100);
 
+    // Calculating the number of coins in need and passing down the remainder.
+    int quartersNeeded = cents / 25;
+    int qRemainder = cents % 25;
+    int dimesNeeded = qRemainder / 10;
+    int dRemainder = qRemainder % 10;
+    int nickelsNeeded = dRemainder / 5;
+    int nRemainder = dRemainder % 5;
+    int penniesNeeded = nRemainder / 1;
+
+    int totalCoins = quartersNeeded + dimesNeeded + nickelsNeeded + penniesNeeded;
+
+    printf("%i\n", totalCoins);
 }
