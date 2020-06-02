2ad34018996f2c648aae83dc576a2675b3173f33
monolithofficial <monolithofficial@users.noreply.github.com>
2020-06-02 15:39:00 +0000

    automated commit by check50 [check50=True]

--- import.py
+++ import.py
@@ -2,41 +2,50 @@ from sys import argv, exit
 import csv
 import sqlite3
 
+# Making sure the number of commandline arguments is correct
 if len(argv) != 2:
     print("GETOUT")
     exit(1)
 
+# Making sure the first commandline argument contains ".csv" file extension
 if ".csv" not in argv[1]:
     print("GETOUT")
     exit(2)
 
+# Connecting to the database via function
+
+
 def connect_db(name):
     return sqlite3.connect(name)
+
 
 conn = connect_db("students.db")
 c = conn.cursor()
 
-index = 1
+# Opening the .csv file
 with open(argv[1], "r") as characters:
     reader = csv.DictReader(characters)
 
+    # Iterating over the rows
     for row in reader:
+        # Splitting "name" to get firs middle and last names
         first = row["name"].split()[0]
         house = row["house"]
         birth = row["birth"]
         print(row["name"].split(), row["house"], row["birth"])
+        # Inserting rows into the database
         if len(row["name"].split()) < 3:
             middle = None
             last = row["name"].split()[1]
-            c.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", (first, middle, last, house, birth))
+            c.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)",
+                      (first, middle, last, house, birth))
         elif len(row["name"].split()) == 3:
             middle = row["name"].split()[1]
             last = row["name"].split()[2]
-            c.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)", (first, middle, last, house, birth))
+            c.execute("INSERT INTO students (first, middle, last, house, birth) VALUES (?, ?, ?, ?, ?)",
+                      (first, middle, last, house, birth))
 
-# for i in c.execute("SELECT * FROM students"):
-#     print(i)
-
+# Commiting and closing the database
 conn.commit()
 conn.close()
 
--- roster.py
+++ roster.py
@@ -1,6 +1,7 @@
 from sys import argv, exit
 import sqlite3
 
+# Making sure the number of commandline arguments is correct
 if len(argv) != 2:
     print("GETOUT")
     exit(1)
@@ -10,15 +11,15 @@ if argv[1].lower() not in houses:
     print("GETOUT")
     exit(1)
 
+# Connecting to the database
 conn = sqlite3.connect("students.db")
 c = conn.cursor()
+# Storing input in a variable for case manipulation purposes
 house_inp = argv[1].lower()
 
+# Inerating over records and printing them out in formatted form
 for row in c.execute("SELECT * FROM students WHERE LOWER(house) = ? ORDER BY last", (house_inp, )):
     if row[2] == None:
         print("{0} {1}, born in {2}".format(row[1], row[3], row[5]))
     else:
         print("{0} {1} {2}, born in {3}".format(row[1], row[2], row[3], row[5]))
-
-
-
